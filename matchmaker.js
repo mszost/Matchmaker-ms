0ae4b5963ba8baba44ca896363153a5aad6a8c05
Michael Szostak <mzostak@gmail.com>
2022-11-18 12:31:17 -0600

    add more questions and a barebones calculate function

---
+++ matchmaker.js
@@ -0,0 +1,24 @@
+/*
+function calculateCompatbility() {
+    const preferedAnswers = [3, 4, 1];
+    console.log('calculating love');
+    var userAnswers = [];
+    userAnswers.push(document.getElementById("q1").selectedOptions[0].value);
+    userAnswers.push(document.getElementById("q2").selectedOptions[0].value);
+    userAnswers.push(document.getElementById("q3").selectedOptions[0].value);
+
+    console.log(userAnswers);
+
+    let totalCompatibilty = 0;
+
+    for (let i = 0; i < userAnswers.length; i++) {
+        var userAnswers = parseInt(userAnswers[i]);
+        let questionCompatibility = 5 - Math.abs(userAnswers - preferedAnswers[i]);
+        totalCompatibilty += questionCompatibility;
+    }
+
+    console.log(totalCompatibilty);
+
+    document.getElementById("score").innerHTML = "Your compatibility is: " + totalCompatibilty;
+    }
+     */
